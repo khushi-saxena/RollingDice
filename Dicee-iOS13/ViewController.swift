d85e16c674bbfc2a4e5f44e1ca741226da24d297
khushi-saxena <khushisaxena123123@gmail.com>
2025-05-17 00:16:18 -0700

    New Changes

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,14 +12,24 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
+    
+    var leftDiceNumber = 1
+    var rightDiceNumber=5
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         //who.what=value
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        //)diceImageView1.alpha=0.5
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        super.viewDidLoad()
     }
 
-
+    @IBAction func rollButtenPressed(_ sender: UIButton) {
+        diceImageView1.image = [ #imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        diceImageView2.image=[#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        leftDiceNumber=leftDiceNumber+1
+        rightDiceNumber=rightDiceNumber-1
+        
+        print(Int.random(in: 1...10))
+    }
+    
 }
 
